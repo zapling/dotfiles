2ed0de9daaa55b57587389effff3fceda671e7d4
Andreas Palm <andreaspalm98@gmail.com>
2020-03-07 00:51:31 +0100

    Setup scripts

--- setup/setup-manjaro.sh
+++ setup/setup-manjaro.sh
@@ -8,8 +8,17 @@ fi
 install_packages() {
     local packages=''
 
+    # drivers
+    packages+=' mesa'
+
     # i3 wm
-    packages+=' i3-gaps i3status-manjaro dmenu-manjaro polybar'
+    packages+=' i3-gaps i3status-manjaro i3-lock dmenu-manjaro polybar'
+
+    # Terminal
+    packages+=' zsh kitty'
+
+    # Fonts
+    package+=' noto-fonts-emoji'
 
     # Misc programs
     packages+=' neofetch'
@@ -17,9 +26,18 @@ install_packages() {
     pacman -Sy --noconfirm $packages
 }
 
-setup_symlinks() {
-    # Touchpad configuration
-    ln -s $PWD/etc-x11-xorg-conf-d/30-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf 
+setup_oh_my_zsh() {
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 }
 
-setup_symlinks
+setup_driver_configs() {
+    # Intel driver configuration
+    ln -s $PWD/etc-x11-xorg-conf-d/20-intel.conf /etc/X11/xorg.conf.d/
+
+    # Touchpad configuration
+    ln -s $PWD/etc-x11-xorg-conf-d/30-touchpad.conf /etc/X11/xorg.conf.d/ 
+}
+
+setup_driver_configs
+install_packages
+setup_oh_my_zsh
--- setup/setup-symlinks.sh
+++ setup/setup-symlinks.sh
@@ -5,18 +5,22 @@ dotfiles=$HOME/dotfiles
 #==========
 # Delete existing dotfiles / symlinks
 #==========
-#rm -rf ~/.zshrc
-#rm -rf ~/.config/i3/
-#rm -rf ~/.config/polybar/
-#rm -rf ~/.config/kitty/
+rm -rf ~/.local/bin/
+rm -rf ~/.zshrc
+rm -rf ~/.config/i3/
+rm -rf ~/.config/polybar/
+rm -rf ~/.config/kitty/
+rm -rf ~/.oh-my-zsh/custom/themes/
 
 #==========
 # Create symlinks
 #==========
+ln -s $dotfiles/bin/ ~/.local/
 ln -s $dotfiles/.zshrc ~/.zshrc
-ln -s $dotfiles/.config/i3 ~/.config/i3/
-ln -s $dotfiles/.config/polybar ~/.config/polybar/
-ln -s $dotfiles/.config/kitty ~/.config/kitty/
+ln -s $dotfiles/.config/i3 ~/.config/
+ln -s $dotfiles/.config/polybar ~/.config/
+ln -s $dotfiles/.config/kitty ~/.config/
+ln -s $dotfiles/.oh-my-zsh/custom/themes/ ~/.oh-my-zsh/custom/
 
 #==========
 # Set zsh as the default shell
