a84d2800342d4ef16003f1bf33150b22bc4899f8
Andreas Palm <andreaspalm98@gmail.com>
2020-08-24 19:37:43 +0200

    Read and write default config

--- .config/i3blocks/scripts/weather-applet/cmd/weatherapplet.go
+++ .config/i3blocks/scripts/weather-applet/cmd/weatherapplet.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"weather-applet/internal"
 )
 
 func main() {
-	fmt.Println(internal.GetConfigurationPath())
+	internal.GetConfig()
 }
--- .config/i3blocks/scripts/weather-applet/internal/config.go
+++ .config/i3blocks/scripts/weather-applet/internal/config.go
@@ -1,14 +1,64 @@
 package internal
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"os"
 )
 
 var configFolder = "weather-applet"
 var configFile = "config.json"
 
-func GetConfig() {
+type Configuration struct {
+	Ssid      string  `json:"ssid"`
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+}
+
+// Default configuration with Gothenburg points
+var defaultConfiguration Configuration = Configuration{
+	Ssid:      "Your wifi name",
+	Latitude:  57.7,
+	Longitude: 11.9,
+}
+
+func GetConfig() []Configuration {
 	path := getConfigDir()
+	exists := os.MkdirAll(path, 0755)
+
+	// could not create or read path
+	if exists != nil {
+		return getDefaultConfig()
+	}
+
+	fullPath := path + "/" + configFile
+	f, err := os.Open(fullPath)
+	// could not find config file
+	if err != nil {
+		writeDefaultConfig(fullPath)
+		return getDefaultConfig()
+	}
+
+	defer f.Close()
+
+	var config []Configuration
+	bytes, _ := ioutil.ReadAll(f)
+	json.Unmarshal(bytes, &config)
+
+	return config
+}
+
+func getDefaultConfig() []Configuration {
+	var config []Configuration
+	return append(config, defaultConfiguration)
+}
+
+func writeDefaultConfig(path string) {
+	config := getDefaultConfig()
+	jsonData, _ := json.Marshal(config)
+	f, _ := os.Create(path)
+	f.Write(jsonData)
+	f.Close()
 }
 
 func getConfigDir() string {
