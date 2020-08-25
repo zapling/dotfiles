f042aba9cec4ba482fdbdc763751b121e54432e3
Andreas Palm <andreaspalm98@gmail.com>
2020-08-25 17:57:38 +0200

    GetCurrentSsid()

--- .config/i3blocks/scripts/weather-applet/internal/weather.go
+++ .config/i3blocks/scripts/weather-applet/internal/weather.go
@@ -1,12 +1,10 @@
 package internal
 
 import (
-	"fmt"
 	"os/exec"
 )
 
-// wip: https://stackoverflow.com/questions/41037870/go-exec-command-run-command-which-contains-pipe
-func GetCurrentSsid(ids []string) {
+func GetCurrentSsid(ids []string) string {
 	nmcli := exec.Command("nmcli", "connection", "show", "--active")
 	pipe, _ := nmcli.StdoutPipe()
 	defer pipe.Close()
@@ -16,25 +14,13 @@ func GetCurrentSsid(ids []string) {
 	for _, v := range ids {
 		grep := exec.Command("grep", v)
 		grep.Stdin = pipe
-		res, _ := grep.Output()
-		fmt.Print(string(res))
+		_, err := grep.Output()
+		if err != nil {
+			continue
+		}
 
+		return v
 	}
 
-	// grep := exec.Command("grep", "bla")
-
-	// pipe, _ = nmcli.StdoutPipe()
-	// defer pipe.Close()
-
-	// grep.Stdin = pipe
-
-	// nmcli.Start()
-	// res, err := grep.Output()
-
-	// if err != nil {
-	// 	fmt.Print(err)
-	// }
-
-	// fmt.Print(string(res))
-
+	return ""
 }
