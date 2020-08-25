29c79c05f58da2052036be73481fb9bf2e62fab8
Andreas Palm <andreaspalm98@gmail.com>
2020-08-25 21:42:12 +0200

    wip: basic config

--- .config/i3blocks/scripts/weather-applet/cmd/weatherapplet.go
+++ .config/i3blocks/scripts/weather-applet/cmd/weatherapplet.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"weather-applet/internal"
 )
 
 func main() {
-	internal.GetCurrentSsid()
+	config := internal.GetConfig()
+	var ids []string
+	for _, v := range config {
+		ids = append(ids, v.Ssid)
+	}
+	ssid := internal.GetCurrentSsid(ids)
+	fmt.Print(ssid)
+	for i := range config {
+		if config[i].Ssid == ssid {
+			fmt.Print(config[i])
+		}
+	}
 }
