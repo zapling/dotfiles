e7064db42fe8787eec18149660eb3bc5daeac952
Andreas Palm <andreaspalm98@gmail.com>
2020-10-19 16:12:34 +0200

    Fixed some invalid instructions

--- setup/setup-terminal.sh
+++ setup/setup-terminal.sh
@@ -7,15 +7,20 @@ packages='kitty zsh'
 sudo pacman -Sy --needed $packages
 
 # Install oh-my-zsh if not installed
-[ -d "~/.oh-my-zsh" ] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+[ -d "~/.oh-my-zsh" ] && \
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Make zsh default shell
-chsh -s $(which zsh) || true
+[[ ! "$SHELL" =~ "zsh" ]] && chsh -s $(which zsh)
 
 # Setup symlinks
-cd ~
-[ ! -L ".zshrc" ] && rm .zshrc && ln -s ~/dotfiles/.zshrc
-cd .oh-my-zsh
-[ ! -L "custom" ] && rm -rf custom && ln -s ~/dotfiles/.oh-my-zsh/custom
+cd ~ && \
+    [ ! -L ".zshrc" ] && \
+    rm .zshrc && \
+    ln -s ~/dotfiles/.zshrc
+
+cd ~/.oh-my-zsh/custom/themes/ && \
+    [ ! -L "sunaku-zapling.zsh-theme" ] && \
+    ln -s ~/dotfiles/.oh-my-zsh/custom/sunaku-zapling.zsh-theme
 
 exit 0
