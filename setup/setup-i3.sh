ab13b62f6b386c385b33ab1ef198dd0e1982bf99
Andreas Palm <andreaspalm98@gmail.com>
2020-12-23 16:12:02 +0100

    Configure i3-autodisplay

--- setup/setup-i3.sh
+++ setup/setup-i3.sh
@@ -3,7 +3,7 @@
 echo -e "\033[1;34mI3 SETUP\033[0m"
 
 # Install packages related to i3
-packages='i3-gaps i3lock-color dmenu-manjaro i3blocks xwallpaper dunst'
+packages='i3-gaps i3lock-color dmenu-manjaro i3blocks xwallpaper dunst go'
 
 # i3blocks script dependencies
 packages+=' acpi sysstat'
@@ -16,10 +16,19 @@ if ! type "xkblayout-state" > /dev/null; then
   pamac build --no-confirm xkblayout-state-git
 fi
 
+# Display settings, auto configure monitors
+if ! type "i3-autodisplay" > /dev/null; then
+    cd /tmp && git clone https://github.com/lpicanco/i3-autodisplay.git && \
+        cd i3-autodisplay && \
+        go build cmd/i3-autodisplay/i3-autodisplay.go && \
+        sudo mv i3-autodisplay /usr/bin/.
+fi
+
 # Setup symlinks to configs
 cd ~/.config/
 [ ! -L "i3" ] && ln -s ~/dotfiles/.config/i3
 [ ! -L "i3blocks" ] && ln -s ~/dotfiles/.config/i3blocks
+[ ! -L "i3-autodisplay" ] && ln -s ~/dotfiles/.config/i3-autodisplay
 [ ! -L "dunst" ] && ln -s ~/dotfiles/.config/dunst
 
 exit 0
