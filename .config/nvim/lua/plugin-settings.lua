367c75f314942843e092a18fc767ff938a4b91f5
Andreas Palm <andreaspalm98@gmail.com>
2021-08-24 19:41:24 +0200

    Auto show lsp diagnostics on the same line

--- .config/nvim/lua/plugin-settings.lua
+++ .config/nvim/lua/plugin-settings.lua
@@ -13,7 +13,6 @@ require('gitsigns').setup{
     topdelete    = {hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
     changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
   },
-  -- wrap = false, bug: https://github.com/lewis6991/gitsigns.nvim/pull/210
   keymaps = {
     ['n ]c'] = { '<cmd>lua require"gitsigns".next_hunk({wrap = false})<CR>'},
     ['n [c'] = { '<cmd>lua require"gitsigns".prev_hunk({wrap = false})<CR>'},
@@ -61,11 +60,9 @@ vim.api.nvim_command('sign define LspDiagnosticsSignWarning text= texthl=LspD
 vim.api.nvim_command('sign define LspDiagnosticsSignInformation text= texthl=LspDiagnosticsSignInformation linehl= numhl=')
 vim.api.nvim_command('sign define LspDiagnosticsSignHint text= texthl=LspDiagnosticsSignHint linehl= numhl=')
 
--- floating windows can be renderd on top of eachother it seems like
--- autocmd CursorHold conflicts with hover().
--- https://github.com/neovim/neovim/issues/11508
--- vim.api.nvim_command('autocmd CursorHold * lua vim.lsp.diagnostic.show_line_diagnostics({focusable = false})')
--- vim.api.nvim_command [[autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()]]
+-- auto show diagnostics
+vim.api.nvim_command('autocmd CursorHold * lua vim.lsp.diagnostic.show_line_diagnostics({focusable = true})')
+vim.api.nvim_command [[autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()]]
 
 -- LSP auto completion
 require'compe'.setup {
@@ -93,19 +90,21 @@ require'compe'.setup {
   };
 }
 
+-- Get current git branch
+-- Longer branch names gets cut off in smaller windows
 local function GitBranchCutOff()
     local branch = vim.api.nvim_eval('fugitive#head()')
     local max_length = 29
 
     local win_width = vim.api.nvim_win_get_width(0)
     if win_width <= 53 then -- dont show branch when the window is small
-	return ''
+        return ''
     elseif win_width < 90 then -- show short branch name
-	max_length = 5
+        max_length = 5
     end
 
     if string.len(branch) > max_length + 1 then
-	return string.sub(branch, 0, max_length)..'~.'
+        return string.sub(branch, 0, max_length)..'~.'
     end
 
     return branch
