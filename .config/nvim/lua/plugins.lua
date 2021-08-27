d574ad04b731efa58e97f30cd4734d81a3ea68c8
Andreas Palm <andreaspalm98@gmail.com>
2021-08-27 21:45:59 +0200

    Comments

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -13,40 +13,39 @@ end
 return require('packer').startup(function()
   use 'wbthomason/packer.nvim'
 
-  use 'tpope/vim-fugitive'              -- Git integration
-  use {
-      'lewis6991/gitsigns.nvim',
-      requires = {'nvim-lua/plenary.nvim'}
-  } 					-- Git annotations
-  use 'ntpeters/vim-better-whitespace'  -- See annoying whitespace
-  use 'joom/vim-commentary'             -- Toggle code comments
-  use 'tpope/vim-surround'              -- Edit 'surroundings'
-  use 'tpope/vim-repeat'                -- . repetition for custom motions
-  use 'FooSoft/vim-argwrap'             -- Wrap function arguments with keypress
-  use 'unblevable/quick-scope'          -- f,F,t,F motion highlighting
+  -- Core (feels like native vim functionality)
+  use 'joom/vim-commentary'            -- Toggle code comments
+  use 'tpope/vim-surround'             -- Edit 'surroundings'
+  use 'tpope/vim-repeat'               -- . repetition for custom motions
+  use 'FooSoft/vim-argwrap'            -- Wrap function arguments with keypress
+  use 'unblevable/quick-scope'         -- f,F,t,F motion highlighting
+  use 'ntpeters/vim-better-whitespace' -- show that fucking whitespace
 
-  -- database viewer
+  -- Git
+  use 'tpope/vim-fugitive'
+  use {'lewis6991/gitsigns.nvim', requires = {'nvim-lua/plenary.nvim'}}
+
+  -- Database viewer
+  -- TODO: remove or get into workflow of using it
   use 'tpope/vim-dadbod'
   use 'kristijanhusak/vim-dadbod-ui'
 
-  -- theme / syntax
+  -- Theme and styling
   use {"npxbr/gruvbox.nvim", requires = {"rktjmp/lush.nvim"}}
-  use {'shadmansaleh/lualine.nvim'} -- updated fork, author seems to be inactive on the main one
+  use {'shadmansaleh/lualine.nvim'} -- updated fork (org-maintainer afk for months)
   -- use {'hoob3rt/lualine.nvim', branch = 'shadmansaleh:master' }
 
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' } -- syntax
-  use 'kyazdani42/nvim-web-devicons'                           -- icons (requires patch fonts (Nerd fonts)
+  -- Syntax
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' } -- the good stuff
+  use 'kyazdani42/nvim-web-devicons'   -- note: requires patched fonts
 
-  -- language stuff
-  use 'teal-language/vim-teal'
-  use 'hashivim/vim-terraform'
+  use 'hashivim/vim-terraform' -- HCL highlighting
 
-  -- lsp
+  -- Language Server Protocol
   use 'neovim/nvim-lspconfig' -- config helper
   use 'hrsh7th/nvim-compe'    -- auto completion
   use 'folke/trouble.nvim'    -- better diagnostic viewer
 
-  -- search / navigation
+  -- Search / Navigation
   use {'nvim-telescope/telescope.nvim', requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}}
-
 end)
