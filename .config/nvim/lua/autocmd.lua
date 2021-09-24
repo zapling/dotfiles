e72444deb3e2776c2423d68fe3f9512a0997674d
Andreas Palm <andreaspalm98@gmail.com>
2021-09-24 14:26:59 +0200

    Auto equalize windows when vim is resized

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -1,2 +1,5 @@
+-- Auto equalize splits when Vim is resized
+vim.api.nvim_command('autocmd VimResized * wincmd =')
+
 -- Dont highlight whitespace in the Trouble buffer
 vim.api.nvim_command('autocmd FileType Trouble DisableWhitespace')
