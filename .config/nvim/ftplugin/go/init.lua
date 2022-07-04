16be463f9cf1460b6e213823c57c15ae5a038f4d
Andreas Palm <andreaspalm98@gmail.com>
2022-07-04 15:55:21 +0200

    [[ and ]] for go should be silent

--- .config/nvim/ftplugin/go/init.lua
+++ .config/nvim/ftplugin/go/init.lua
@@ -1,15 +1,15 @@
 local function quick_jump(direction)
     local search_prefix = '?'
-    local regex = [[^\(func\|var\|type\|import\)]]
+    local regex = [[^\(package\|import\|type\|const\|var\|func\)]]
 
     if direction == 'down' then
         search_prefix = '/'
     end
 
-    local search = search_prefix .. regex
+    local search = search_prefix .. regex .. search_prefix
 
-    vim.api.nvim_exec(search, nil)
-    vim.api.nvim_exec('nohls', nil)
+    vim.api.nvim_exec('silent ' .. search, nil)
+    vim.api.nvim_exec('silent nohls', nil)
 end
 
 vim.keymap.set('n', '[[', function() quick_jump('up') end, { silent = true, buffer = true})
