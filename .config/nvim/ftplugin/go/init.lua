ae4fba836c86e646cf08b1d44f014f3d8411d373
Andreas Palm <andreaspalm98@gmail.com>
2022-07-06 14:38:11 +0200

    [[ and ]] jump in go without polluting search history

--- .config/nvim/ftplugin/go/init.lua
+++ .config/nvim/ftplugin/go/init.lua
@@ -1,16 +1,14 @@
 local function quick_jump(direction)
-    local search_prefix = '?'
-    local regex = [[^\(package\|import\|type\|const\|var\|func\)]]
+    local flags = 'b'
+    local regex = [[^\\(package\\|import\\|type\\|const\\|var\\|func\\)]]
 
     if direction == 'down' then
-        search_prefix = '/'
+        flags = ''
     end
 
-    local search = search_prefix .. regex .. search_prefix
+    local command = string.format('execute search("%s", "%s")', regex, flags)
 
-    vim.api.nvim_exec('silent ' .. search, nil)
-    vim.api.nvim_exec('silent nohls', nil)
-    vim.api.nvim_exec('norm 0', nil)
+    vim.api.nvim_exec(command, nil)
 end
 
 vim.keymap.set('n', '[[', function() quick_jump('up') end, { silent = true, buffer = true})
