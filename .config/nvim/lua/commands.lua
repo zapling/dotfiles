aa6ce8a3011b0ec5b343b60970faaaf234d95a74
Andreas Palm <andreaspalm98@gmail.com>
2023-03-24 16:33:15 +0100

    Added :GitOpen command to show the current file and line in gitlab/github

--- .config/nvim/lua/commands.lua
+++ .config/nvim/lua/commands.lua
@@ -16,6 +16,16 @@ local function get_current_branch()
     return branch
 end
 
+local function get_substr(text, pattern)
+    local i, j = string.find(text, pattern)
+
+    if not i then
+        return ''
+    end
+
+    return text:sub(i, j)
+end
+
 local git_rebase_current_branch = function()
     local current_branch = get_current_branch()
     if current_branch == nil then
@@ -94,6 +104,66 @@ local function insert_timestamp()
     vim.cmd('normal i' .. timestamp)
 end
 
+local function show_file_in_gitlab()
+    local filepath = vim.fn.expand('%')
+    local linenum = vim.fn.line('.')
+
+    local origin = nil
+    Job:new({
+      command = 'git',
+      args = { 'config', '--get', 'remote.origin.url' },
+      on_exit = function(j, return_val)
+        if return_val ~= 0 then
+            return
+        end
+
+        origin = j:result()[1]
+      end,
+    }):sync()
+
+    if origin == nil then
+        print("Not in a git repo")
+        return
+    end
+
+    local host = get_substr(origin, "@.+:")
+    local namespace = get_substr(origin, ":.+/")
+    local repo = get_substr(origin, "/.+%.git")
+
+    if host == "" or namespace == "" or repo == "" then
+        return ""
+    end
+
+    host = host:sub(2, host:len()-1)                -- remove starting "@" and ending ":"
+    namespace = namespace:sub(2, namespace:len()-1) -- remove starting ":" and ending "/"
+    repo = repo:sub(2, repo:len()-4)                -- remove starting "/" and ending ".git"
+
+    local head_sha1 = nil
+    Job:new({
+      command = 'git',
+      args = { 'rev-parse', 'HEAD'},
+      on_exit = function(j, return_val)
+        if return_val ~= 0 then
+            return
+        end
+
+        head_sha1 = j:result()[1]
+      end,
+    }):sync()
+
+    local link = 'https://' .. host .. '/' .. namespace .. '/' .. repo
+
+    if string.find(host, "gitlab") then
+        link = link .. '/-'
+    end
+
+    link = link .. '/tree/' .. head_sha1 .. '/'
+    link = link .. filepath .. '#L' .. linenum
+
+    Job:new({command = 'xdg-open', args = { link }}):sync()
+end
+
 vim.api.nvim_create_user_command('Gitrebase', git_rebase_current_branch, {})
+vim.api.nvim_create_user_command('GitOpen', show_file_in_gitlab, {})
 vim.api.nvim_create_user_command('UUIDGen', insert_uuid, {})
 vim.api.nvim_create_user_command('TimestampUTC', insert_timestamp, {})
