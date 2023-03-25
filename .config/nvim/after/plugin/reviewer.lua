17495849b90e149917d5025e497cf36f604c6ddd
Andreas Palm <andreaspalm98@gmail.com>
2023-03-25 16:30:07 +0100

    Add custom gitlab instance

--- .config/nvim/after/plugin/reviewer.lua
+++ .config/nvim/after/plugin/reviewer.lua
@@ -1,1 +1,19 @@
-require("reviewer").setup()
+require("reviewer").setup({
+    providers = {
+        zimpler_gitlab = {
+            host = "gitlab.zimpler.com",
+            get_provider = function()
+                return require('reviewer.provider.gitlab')
+            end,
+            opts = {
+                base_url = 'https://gitlab.zimpler.com',
+                access_token = os.getenv('ZIMPLER_GITLAB_REVIEWER_TOKEN'),
+            }
+        }
+    },
+    debug = {
+        logger = {
+            use_file = true,
+        }
+    },
+})
